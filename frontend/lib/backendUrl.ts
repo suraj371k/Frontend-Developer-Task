ba24120320ec4abebfdbd27583a961ce41147083
Suraj Kushwaha <119525498+suraj371k@users.noreply.github.com>
2026-01-01 03:47:47 +0530

    Update backendUrl.ts

--- frontend/lib/backendUrl.ts
+++ frontend/lib/backendUrl.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://frontend-developer-task-ko2k.onrender.com",
   withCredentials: true,
 });
